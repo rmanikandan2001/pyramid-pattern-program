8a8c052dcf168db86d86e20f4f8f1836d5be3373
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-24 23:43:17 -0700

    add

---
+++ main.js
@@ -0,0 +1,18 @@
+
+	var i,j,k;
+	for(i=1; i<=5; i++){
+		
+		for(k=1; k<=(5-i); k++){
+		
+			document.write("&nbsp");
+		}	
+	
+		for(j=1; j<=i; j++){
+		
+		document.write("*");
+		
+		}
+		
+		document.write("<br>");
+		
+		}
